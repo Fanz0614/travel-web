7fcb2c402944d35f2c80af25a8fd5a586ef9fedb
Jacky <Jackyzh0614@gmail.com>
2021-10-08 13:17:45 +1100

    change token

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import "antd/dist/antd.css";
-import "./i18n/configs";
-import { Provider } from "react-redux";
-import rootStore from "./redux/store";
-import axios from "axios";
-import { PersistGate } from "redux-persist/integration/react";
-import {Loading} from './components/loading'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'antd/dist/antd.css';
+import './i18n/configs';
+import { Provider } from 'react-redux';
+import rootStore from './redux/store';
+import axios from 'axios';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Loading } from './components/loading';
 
-axios.defaults.headers["x-icode"] = "8FFD2D9581256CE8";
+axios.defaults.headers['x-icode'] = '5BCE0B2ED39B976B';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={rootStore.store}>
-      <PersistGate loading={<Loading/>}persistor={rootStore.persistor}>
+      <PersistGate loading={<Loading />} persistor={rootStore.persistor}>
         <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
